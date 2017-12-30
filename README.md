cc3cb2835e7a80d59cc883557ecb98318a77eacb
Noob <965768042@qq.com>
2017-12-30 21:30:41 +0800

    BlogClient

---
+++ README.md
@@ -0,0 +1,11 @@
+# React-cli
+React-typescript-webpack cli
+
+# usage
+
+> dev
+```
+$ git clone https://github.com/ly321/react-typescript-webpack.git
+$ npm install
+$ npm run dev/ npm run start
+```
---
+++ build/webpack.base.conf.js
@@ -0,0 +1,29 @@
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const path=require('path');
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+      filename: "bundle.js",
+      path:path.resolve(__dirname, "../dist"),
+    },
+  
+    devtool: "source-map",
+  
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+  
+    module: {
+      rules: [
+        { test: /\.tsx?$/, loader: "ts-loader" },
+        { test: /\.css$/, loader: 'style-loader!css-loader' },
+        {test: /\.scss$/,loader: 'style!css!sass'},
+        {test: /\.(png|jpeg|gif|jpg)$/,loader: 'url?limit=25000'},
+        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      ]
+    },
+  
+    plugins:[
+      new OpenBrowserPlugin({url: 'http://localhost:3000/'})
+  ]
+  };
