1e3b54ccc8d45956c55cd741e30c0e62719a73b7
ly321 <965768042@qq.com>
2018-01-11 14:51:03 +0800

    find a problem:how to get user data?

--- main.js
+++ main.js
@@ -14,7 +14,10 @@ function createWindow () {
   // 创建浏览器窗口。
   win = new BrowserWindow({width: 1200, height: 800,
     resizable:false,
-     frame: false
+     frame: false,
+     webPreferences:{
+      // devTools:false,
+     }
   })
   
   
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import "./login.scss";
 import {shell} from 'electron';
- class Login extends React.Component{
+import 'whatwg-fetch';
+class Login extends React.Component{
     goGithub(e:any){
         e.preventDefault();
         shell.openExternal('https://github.com/join');
+    }
+    login(){
+        window.location.href="https://github.com/login/oauth/authorize?client_id=0cacf631e36a12692407";
     }
     render(){
         return(
@@ -24,12 +28,12 @@ import {shell} from 'electron';
                     <p>The best way to build and ship software. <a className="click" href="#" onClick={this.goGithub}>Go to github.com</a> to sign up for an account</p>
                 </div>
                 <div className="btns">
-                    <p>
-                        <span><img src="./src/assets/img/complete-r-o.png" alt=""/></span>
+                    <p className="click" onClick={this.login}>
+                        <img src="./src/assets/img/complete-r-o.png" alt=""/>
                         <span>Log in</span>
                     </p>
-                    <p> 
-                        <span><img src="./src/assets/img/error-r-o.png" alt=""/></span>
+                    <p className="click"> 
+                        <img src="./src/assets/img/error-r-o.png" alt=""/>
                         <span>Cancel</span>
                     </p>
                 </div> 
