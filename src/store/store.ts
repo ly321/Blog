90755c8e42e2e21280116d3f4451aa370e8ba3a4
sillyY <965768042@qq.com>
2018-04-10 14:55:50 +0800

    modify style

--- src/store/store.ts
+++
@@ -1,6 +0,0 @@
-import {createStore,applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import * as promise from 'redux-promise';
-import reducer from './reducer';
-
-export default createStore(reducer,applyMiddleware(thunk,promise));
